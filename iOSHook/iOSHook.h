1ab65dd2c89a17bc2b08801d90c33213acbe4f3d
Yanni <yanni.wang@shopee.com>
2019-10-01 19:08:25 +0800

    no message

---
+++ iOSHook/iOSHook.h
@@ -0,0 +1,19 @@
+//
+//  iOSHook.h
+//  iOSHook
+//
+//  Created by Yanni Wang on 1/10/19.
+//  Copyright © 2019 Yanni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSHook.
+FOUNDATION_EXPORT double iOSHookVersionNumber;
+
+//! Project version string for iOSHook.
+FOUNDATION_EXPORT const unsigned char iOSHookVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSHook/PublicHeader.h>
+
+

8f4dec02cf92a2c2daa01e8db2a9609a2079668f
Yanni <yanni.wang@shopee.com>
2019-10-04 17:59:42 +0800

    no message

--- AspectsTests/AspectsInstanceBeforeTests.m
+++ AspectsTests/AspectsInstanceBeforeTests.m
@@ -9,32 +9,15 @@
 #import <XCTest/XCTest.h>
 #import "Aspects.h"
 
-@interface Parent : NSObject
+@interface TestObject : NSObject
 
-- (void)parentMethod:(BOOL *)executed;
-
-@end
-
-@implementation Parent
-
-- (void)parentMethod:(BOOL *)executed
-{
-    if (executed) {
-        *executed = YES;
-    }
-}
+- (void)someMethod:(BOOL *)executed;
 
 @end
 
-@interface Child : NSObject
+@implementation TestObject
 
-- (void)childMethod:(BOOL *)executed;
-
-@end
-
-@implementation Child
-
-- (void)childMethod:(BOOL *)executed;
+- (void)someMethod:(BOOL *)executed;
 {
     if (executed) {
         *executed = YES;
@@ -49,42 +32,43 @@
 
 @implementation AspectsTests
 
-- (void)testNoHook {
-    Child *obj = [[Child alloc] init];
+- (void)testNoHook
+{
+    TestObject *obj = [[TestObject alloc] init];
     BOOL executed = NO;
-    [obj childMethod:&executed];
+    [obj someMethod:&executed];
     XCTAssert(executed == YES);
 }
 
-- (void)testTriggered {
+- (void)testTriggered
+{
     NSError *error = nil;
-    Child *obj = [[Child alloc] init];
+    TestObject *obj = [[TestObject alloc] init];
     __block BOOL triggered = NO;
     
-    [obj aspect_hookSelector:@selector(childMethod:) withOptions:AspectPositionBefore usingBlock:^(id<AspectInfo> info){
+    [obj aspect_hookSelector:@selector(someMethod:) withOptions:AspectPositionBefore usingBlock:^(id<AspectInfo> info){
         triggered = YES;
     } error:&error];
     XCTAssert(error == nil);
     
     XCTAssert(triggered == NO);
-    [obj childMethod:NULL];
+    [obj someMethod:NULL];
     XCTAssert(triggered == YES);
 }
 
-- (void)testOrder {
+- (void)testOrder
+{
     NSError *error = nil;
-    Child *obj = [[Child alloc] init];
+    TestObject *obj = [[TestObject alloc] init];
     __block BOOL executed = NO;
     
-    [obj aspect_hookSelector:@selector(childMethod:) withOptions:AspectPositionBefore usingBlock:^(id<AspectInfo> info){
+    [obj aspect_hookSelector:@selector(someMethod:) withOptions:AspectPositionBefore usingBlock:^(id<AspectInfo> info){
         XCTAssert(executed == NO);
     } error:&error];
     XCTAssert(error == nil);
     
-    [obj childMethod:&executed];
+    [obj someMethod:&executed];
     XCTAssert(executed == YES);
 }
-
-
 
 @end

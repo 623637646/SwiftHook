9b092d96e9c4bc1dbe44a2494aade227d7da8bdb
Yanni <yanni.wang@shopee.com>
2020-05-21 00:52:40 +0800

    HookContextTests

---
+++ SwiftHookTests/HookContextTests.swift
@@ -0,0 +1,79 @@
+//
+//  HookContextTests.swift
+//  SwiftHookTests
+//
+//  Created by Yanni Wang on 21/5/20.
+//  Copyright © 2020 Yanni. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftHook
+
+class HookContextTests: XCTestCase {
+    
+    let InternalErrorNoMethod = 209
+    
+    func testNoMethod() {
+        do {
+            _ = try HookContext.init(targetClass: TestObject.self, selector: #selector(SuperObject.superFunc(arg:)))
+            XCTAssertTrue(false)
+        } catch SwiftHookError.internalError(file: let file, line: let line) {
+            XCTAssertTrue(file.hasSuffix("HookContext.swift"))
+            XCTAssertEqual(line, InternalErrorNoMethod)
+        } catch {
+            XCTAssertNil(error)
+        }
+    }
+    
+    func testHook() {
+        do {
+            var result = [Int]()
+            let context = try HookContext.init(targetClass: TestObject.self, selector: #selector(TestObject.execute(closure:)))
+            XCTAssertTrue(context.isHoolClosurePoolEmpty())
+            
+            try context.append(hookClosure: {
+                result.append(-1)
+                } as @convention(block) () -> Void as AnyObject, mode: .before)
+            XCTAssertFalse(context.isHoolClosurePoolEmpty())
+            
+            try context.append(hookClosure: {
+                result.append(-2)
+                } as @convention(block) () -> Void as AnyObject, mode: .before)
+            XCTAssertFalse(context.isHoolClosurePoolEmpty())
+            
+            try context.append(hookClosure: { original, execute in
+                result.append(-3)
+                original(execute)
+                result.append(3)
+                } as @convention(block) (@escaping (() -> Void) -> Void, () -> Void) -> Void as AnyObject, mode: .instead)
+            XCTAssertFalse(context.isHoolClosurePoolEmpty())
+            
+            try context.append(hookClosure: { original, execute in
+                result.append(-4)
+                original(execute)
+                result.append(4)
+                } as @convention(block) (@escaping (() -> Void) -> Void, () -> Void) -> Void as AnyObject, mode: .instead)
+            XCTAssertFalse(context.isHoolClosurePoolEmpty())
+            
+            try context.append(hookClosure: {
+                result.append(1)
+                } as @convention(block) () -> Void as AnyObject, mode: .after)
+            XCTAssertFalse(context.isHoolClosurePoolEmpty())
+            
+            try context.append(hookClosure: {
+                result.append(2)
+                } as @convention(block) () -> Void as AnyObject, mode: .after)
+            XCTAssertFalse(context.isHoolClosurePoolEmpty())
+            
+            let object = TestObject()
+            XCTAssertEqual(result, [])
+            object.execute {
+                result.append(0)
+            }
+            XCTAssertEqual(result, [-2, -1, -4, -3, 0, 3, 4, 2, 1])
+        } catch {
+            XCTAssertNil(error)
+        }
+    }
+    
+}

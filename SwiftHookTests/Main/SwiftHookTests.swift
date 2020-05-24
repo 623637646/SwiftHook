abf271e32c163400379768cc68edcfc3edba8ebd
Yanni <yanni.wang@shopee.com>
2020-05-24 22:51:51 +0800

    Tests

--- SwiftHookTests/Main/SwiftHookTests.swift
+++ SwiftHookTests/Main/SwiftHookTests.swift
@@ -12,6 +12,62 @@ import XCTest
 // TODO: some complicated test cases
 class SwiftHookTests: XCTestCase {
     
+    func testSingleHookBefore() {
+        let testObject = TestObject()
+        let token = try? hookBefore(object: testObject, selector: #selector(TestObject.noArgsNoReturnFunc)) {
+            // run your code
+            print("hooked!")
+        }
+        testObject.noArgsNoReturnFunc()
+        token?.cancelHook() // cancel the hook
+    }
+    
+    func testSingleHookAfterWithArguments() {
+        let testObject = TestObject()
+        let token = try? hookAfter(object: testObject, selector: #selector(TestObject.sumFunc(a:b:)), closure: { a, b in
+            // get the arguments of the function
+            print("arg1 is \(a)") // arg1 is 3
+            print("arg1 is \(b)") // arg1 is 4
+        } as @convention(block) (Int, Int) -> Void)
+        _ = testObject.sumFunc(a: 3, b: 4)
+        token?.cancelHook() // cancel the hook
+    }
+    
+    func testSingleHookInstead() {
+        let testObject = TestObject()
+        let token = try? hookInstead(object: testObject, selector: #selector(TestObject.sumFunc(a:b:)), closure: { original, a, b in
+            // get the arguments of the function
+            print("arg1 is \(a)") // arg1 is 3
+            print("arg1 is \(b)") // arg1 is 4
+            
+            // run original function
+            let result = original(a, b)
+            print("original result is \(result)") // result = 7
+            return 9
+        } as @convention(block) ((Int, Int) -> Int, Int, Int) -> Int)
+        let result = testObject.sumFunc(a: 3, b: 4) // result
+        print("hooked result is \(result)") // result = 9
+        token?.cancelHook() // cancel the hook
+    }
+    
+    func testAllInstances() {
+        let token = try? hookBefore(targetClass: TestObject.self, selector: #selector(TestObject.noArgsNoReturnFunc)) {
+            // run your code
+            print("hooked!")
+        }
+        TestObject().noArgsNoReturnFunc()
+        token?.cancelHook() // cancel the hook
+    }
+    
+    func testClassMethod() {
+        let token = try? hookClassMethodBefore(targetClass: TestObject.self, selector: #selector(TestObject.classMethodNoArgsNoReturnFunc)) {
+            // run your code
+            print("hooked!")
+        }
+        TestObject.classMethodNoArgsNoReturnFunc()
+        token?.cancelHook() // cancel the hook
+    }
+    
 }
 
 // TODO: performance tests measureBlock

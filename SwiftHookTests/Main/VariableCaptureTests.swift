02bcc62e3ddd73b67abeeaedc56deac93febecfa
Yanni <yanni.wang@shopee.com>
2020-09-29 11:15:57 +0800

    TestCases

---
+++ SwiftHookTests/Main/VariableCaptureTests.swift
@@ -0,0 +1,32 @@
+//
+//  VariableCaptureTests.swift
+//  SwiftHookTests
+//
+//  Created by Yanni Wang on 29/9/20.
+//  Copyright © 2020 Yanni. All rights reserved.
+//
+
+import XCTest
+import SwiftHook
+
+class VariableCaptureTests: XCTestCase {
+
+    func test_Int() throws {
+        class MyObject {
+            @objc dynamic func double(number: Int) -> Int {
+                return number * 2
+            }
+        }
+        
+        let object = MyObject()
+        let addNumber = 3
+        let token = try hookInstead(object: object, selector: #selector(MyObject.double(number:)), closure: {original, o, s, number in
+            let result = original(o, s, number)
+            XCTAssertEqual(result, 22)
+            return result + addNumber
+        } as @convention(block) ((AnyObject, Selector, Int) -> Int, AnyObject, Selector, Int) -> Int)
+        XCTAssertEqual(object.double(number: 11), 25)
+        token.cancelHook()
+    }
+
+}

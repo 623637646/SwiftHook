0a82783673731e3994597f0e76553567bbd1b1dd
yanni.wang@shopee.com <YanniWang>
2021-01-06 14:41:07 +0800

    Demo

--- SwiftHookDemo/SwiftHookDemo/AppDelegate.swift
+++ SwiftHookDemo/SwiftHookDemo/AppDelegate.swift
@@ -11,8 +11,15 @@ import EasySwiftHook
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow.init(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController.init()
+        window.makeKeyAndVisible()
+        self.window = window
         
         // hook
         do {
@@ -23,20 +30,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         } catch {}
         
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
 }

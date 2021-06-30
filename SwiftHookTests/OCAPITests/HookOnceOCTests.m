0ad32e530484d3da87982a362d7e132377fdf49a
Yanni Wang <yanni.wang@yannis-mbp.lan>
2021-06-30 21:45:17 +0800

    Test cases for hooking once.

---
+++ SwiftHookTests/OCAPITests/HookOnceOCTests.m
@@ -0,0 +1,83 @@
+//
+//  HookOnceOCTests.m
+//  SwiftHookTests
+//
+//  Created by Yanni Wang on 30/6/21.
+//  Copyright © 2021 Yanni. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+@import SwiftHook;
+
+@interface MyObject_HookOnceOCTests : NSObject
+@property (nonatomic, assign) BOOL run;
+@end
+
+@implementation MyObject_HookOnceOCTests
+
+-(void)myMethod
+{
+    self.run = YES;
+}
+
+@end
+
+@interface HookOnceOCTests : XCTestCase
+
+@end
+
+@implementation HookOnceOCTests
+
+- (void)test_specific_object
+{
+    MyObject_HookOnceOCTests *obj = [[MyObject_HookOnceOCTests alloc] init];
+    __block BOOL run = NO;
+    NSError *error = nil;
+    __block OCToken *token = [obj sh_hookBeforeSelector:@selector(myMethod) error:&error closure:^{
+        run = YES;
+        [token cancelHook];
+    }];
+    XCTAssertNil(error);
+    
+    XCTAssertFalse(obj.run);
+    XCTAssertFalse(run);
+    [obj myMethod];
+    XCTAssertTrue(obj.run);
+    XCTAssertTrue(run);
+    
+    obj.run = NO;
+    run = NO;
+    XCTAssertFalse(obj.run);
+    XCTAssertFalse(run);
+    [obj myMethod];
+    XCTAssertTrue(obj.run);
+    XCTAssertFalse(run);
+}
+
+- (void)test_objects
+{
+    __block BOOL run = NO;
+    NSError *error = nil;
+    __block OCToken *token = [MyObject_HookOnceOCTests sh_hookBeforeSelector:@selector(myMethod) error:&error closure:^{
+        run = YES;
+        [token cancelHook];
+    }];
+    XCTAssertNil(error);
+    
+    MyObject_HookOnceOCTests *obj1 = [[MyObject_HookOnceOCTests alloc] init];
+    XCTAssertFalse(obj1.run);
+    XCTAssertFalse(run);
+    [obj1 myMethod];
+    XCTAssertTrue(obj1.run);
+    XCTAssertTrue(run);
+    
+    run = NO;
+    MyObject_HookOnceOCTests *obj2 = [[MyObject_HookOnceOCTests alloc] init];
+    XCTAssertFalse(obj2.run);
+    XCTAssertFalse(run);
+    [obj2 myMethod];
+    XCTAssertTrue(obj2.run);
+    XCTAssertFalse(run);
+}
+
+@end
---
+++ SwiftHookTests/SwiftAPITests/HookOnceTests.swift
@@ -0,0 +1,74 @@
+//
+//  HookOnceTests.swift
+//  SwiftHookTests
+//
+//  Created by Yanni Wang on 30/6/21.
+//  Copyright © 2021 Yanni. All rights reserved.
+//
+
+import XCTest
+import SwiftHook
+
+class HookOnceTests: XCTestCase {
+
+    func test_specific_object() throws {
+        class MyObject {
+            var run = false
+            @objc dynamic func myMethod() {
+                self.run = true
+            }
+        }
+        
+        let obj = MyObject()
+        var run = false
+        var token: Token?
+        token = try hookBefore(object: obj, selector: #selector(MyObject.myMethod)) {
+            run = true
+            token?.cancelHook()
+        }
+        XCTAssertFalse(obj.run)
+        XCTAssertFalse(run)
+        obj.myMethod()
+        XCTAssertTrue(obj.run)
+        XCTAssertTrue(run)
+        
+        obj.run = false
+        run = false
+        XCTAssertFalse(obj.run)
+        XCTAssertFalse(run)
+        obj.myMethod()
+        XCTAssertTrue(obj.run)
+        XCTAssertFalse(run)
+    }
+    
+    func test_objects() throws {
+        class MyObject {
+            var run = false
+            @objc dynamic func myMethod() {
+                self.run = true
+            }
+        }
+        
+        var run = false
+        var token: Token?
+        token = try hookBefore(targetClass: MyObject.self, selector: #selector(MyObject.myMethod)) {
+            run = true
+            token?.cancelHook()
+        }
+        let obj1 = MyObject()
+        XCTAssertFalse(obj1.run)
+        XCTAssertFalse(run)
+        obj1.myMethod()
+        XCTAssertTrue(obj1.run)
+        XCTAssertTrue(run)
+        
+        let obj2 = MyObject()
+        run = false
+        XCTAssertFalse(obj2.run)
+        XCTAssertFalse(run)
+        obj2.myMethod()
+        XCTAssertTrue(obj2.run)
+        XCTAssertFalse(run)
+    }
+
+}
